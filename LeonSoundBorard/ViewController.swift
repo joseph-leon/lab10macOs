e880ab508548f1e08c77d6be281c215e5d0b0994
joseph-leon <joseph.leon@tecsup.edu.pe>
2023-05-24 10:45:49 -0500

    Initial Commit

---
+++ LeonSoundBorard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeonSoundBorard
+//
+//  Created by Mac 18 on 24/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
